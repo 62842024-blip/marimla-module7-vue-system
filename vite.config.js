581e3137d3b11da6e33af1c0a6001c708f231558
Kimberly Marimla <62842024@chcc.edu.ph>
2026-09-06 15:56:31 +0800

    ci: configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/marimla-module7-vue-system/',
   plugins: [vue(), tailwindcss()],
   test: {
     environment: 'jsdom',
